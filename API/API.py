97f573f574862b465634e70683c7a3f5628c418f
Eugene <resolweru@mail.ru>
2022-05-11 10:31:15 +1000

    fix data return type

--- API/API.py
+++ API/API.py
@@ -7,27 +7,38 @@ class API(object):
         self.__request = request
 
     @classmethod
-    def __parse_args_for_prediction(cls, request) -> tuple[str, str]:
+    def __get_args_for_prediction(cls, request) -> tuple[str, str]:
         TEAM_A: str = request.args["team_A"].replace('_', ' ')
         TEAM_B: str = request.args["team_B"].replace('_', ' ')
         return TEAM_A, TEAM_B
 
     @classmethod
-    def __parse_args_for_calculation(cls, request) -> str:
+    def __predict_score(cls, request) -> str:
+        TrainModel()
+        parsed_args: tuple[str, str] = API.__get_args_for_prediction(request=request)
+        PREDICTED_SCORE: str = str(round(float(PredictScore(*parsed_args).predict), 2))
+        return PREDICTED_SCORE
+
+    @classmethod
+    def __get_args_for_calculation(cls, request) -> float:
+        print(request)
         PREDICTED_SCORE: float = float(request.args["predicted_score"].replace('_', ' '))
+        return PREDICTED_SCORE
+
+    @classmethod
+    def __calculate_advance(cls, request) -> str:
+        PREDICTED_SCORE: float = API.__get_args_for_calculation(request=request)
         if PREDICTED_SCORE > 0:
             return str(PREDICTED_SCORE - 0.5)
         elif PREDICTED_SCORE < 0:
             return str(PREDICTED_SCORE + 0.5)
 
     @property
-    def predict_score(self) -> str:
-        TrainModel()
-        parsed_args: tuple[str, str] = self.__parse_args_for_prediction(request=self.__request)
-        PREDICTED_SCORE: str = str(round(float(PredictScore(*parsed_args).predict), 2))
-        return PREDICTED_SCORE
+    def advance(self):
+        PARSED_ARG: str = self.__calculate_advance(request=self.__request)
+        return PARSED_ARG
 
     @property
-    def calculate_advance(self):
-        PARSED_ARG: str = self.__parse_args_for_calculation(request=self.__request)
-        return PARSED_ARG
+    def predicted_score(self) -> str:
+        return self.__predict_score(request=self.__request)
+
